7bc03d3421369dff5d6ad08ccbb32a415cecaf3d
mohamed.aiman <mohamed.aiman@aasandha.mv>
2016-11-10 14:47:17 +0500

    initial commit

---
+++ src/NilPortugues/Laravel5/JsonApi/Providers/Laravel53Provider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author: Mohamed Aiman <muhammadhu.aiman@aasandha.mv>
+ * Date: 20/10/16
+ * Time: 12:57
+ *
+ */
+
+namespace NilPortugues\Laravel5\JsonApi\Providers;
+
+use Illuminate\Container\Container;
+use Illuminate\Routing\RouteCollection;
+use Illuminate\Routing\UrlGenerator;
+use ReflectionClass;
+
+/**
+ * Class Laravel53Provider.
+ */
+class Laravel53Provider extends Laravel52Provider
+{
+}
